9bb61f9a2a4895c3d3f2c3f7ec23f6c8fabc3100
Prince-hue <atsrimpryn@gmail.com>
2024-02-05 16:48:49 +0000

    install openpyxl and done learning about buttons

--- 05_buttons.py
+++ 05_buttons.py
@@ -1,6 +1,33 @@
 import streamlit as st
+import pandas as pd
 
 if st.button('Say Hello'):
     st.write('Hey Hello There!')
 else:
     st.write('Goodbye')
+    
+text = "Some Texts that will be downloaded."
+st.download_button('Download text', text)
+
+with open("static\\flower.jpg", "rb") as file:
+    btn = st.download_button(
+        label= "Download Image",
+        data = file,
+        file_name = "flower.jpg",
+        mime = "image/jpg"
+    )
+    
+@st.cache_data
+
+def convert_df(df):
+    return df.to_csv().encode('utf-8')
+
+df1 = pd.read_excel('static\\book1.xlsx')
+csv = convert_df(df1)
+
+st.download_button(
+    label = "Download data as csv",
+    data = csv,
+    file_name = "large_df.csv",
+    mime = "text/csv"
+)
